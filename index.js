1b85dd6d15b1d9dcc0c37651bd2cea2c2f1f2c09
Giuseppe <giuseppemiro@gmail.com>
2020-12-09 17:35:25 +0100

    add a maximum of duration to avoid telegram limitation

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const token = process.env.TELEGRAM_TOKEN;
 
 const bot = new TelegramBot(token, { polling: true });
 
+const filename = "audio.mp3";
+const maxDuration = 2917;
+
 bot.onText(/\/echo (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = match[1];
@@ -19,10 +22,30 @@ bot.on("message", (msg) => {
   const chatId = msg.chat.id;
 
   if (msgIsValidUrl(msg.text)) {
-    downloadVideo(msg.text, () => {
-      bot.sendMessage(chatId, "Downloading and converting... please wait.");
-      bot.sendAudio(chatId, "./audio.mp3").then(() => {
-        fs.unlinkSync("./audio.mp3");
+    durationTest(msg.text, (error) => {
+      if (error)
+        return bot.sendMessage(
+          chatId,
+          `Sorry, the video duration should be less than ${
+            maxDuration / 60
+          } minutes`
+        );
+
+      downloadVideo(msg.text, () => {
+        bot
+          .sendAudio(chatId, `./${filename}`)
+          .then(() => {
+            console.log("file send!");
+            fs.unlinkSync(`./${filename}`);
+          })
+          .catch((e) => {
+            console.log("error sending the file");
+            bot.sendMessage(
+              chatId,
+              "Something was wrong please try again with a smaller video"
+            );
+            fs.unlinkSync(`./${filename}`);
+          });
       });
     });
   } else {
@@ -40,10 +63,33 @@ function msgIsValidUrl(msg) {
 }
 
 async function downloadVideo(videoUrl, callback) {
+  bot.sendMessage(chatId, "Downloading and converting... please wait.");
   const { stdout, stderr } = await exec(
-    `youtube-dl -x --audio-format mp3 --output audio.mp3 ${videoUrl}`
+    `youtube-dl -x --output ${filename} --audio-format mp3 --max-filesize 70m ${videoUrl} `
   );
+
   if (!stderr && callback) callback();
+}
+
+async function durationTest(videoUrl, callback) {
+  const { stdout, stderr } = await exec(
+    `youtube-dl -s --get-duration ${videoUrl} `
+  );
+  duration = hmsToSeconds(stdout);
+  if (!stderr && callback) callback(duration > maxDuration);
+}
+
+function hmsToSeconds(str) {
+  var p = str.split(":"),
+    s = 0,
+    m = 1;
+
+  while (p.length > 0) {
+    s += m * parseInt(p.pop(), 10);
+    m *= 60;
+  }
+
+  return s;
 }
 
 bot.on("polling_error", (err) => console.log(err));
